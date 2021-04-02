b9b15ddc6009a64d5f54d974600aedbbbde8bbda
Arthur Pereira <55847858+Artotim@users.noreply.github.com>
2021-04-02 18:45:46 -0300

    Handle "Accept Cookies" button
    When I was about to get the badge, StackOverflow added an accept cookies button and the bot couldn't login to the site. I lost all my progress :( So I added a step to handle that button and also notify the user when login fails. Also adjusted the format.

--- app.js
+++ app.js
@@ -36,46 +36,59 @@ async function crawl() {
     });
     const page = await browser.newPage();
 
+
     console.log(`Loading ${LOGIN_PAGE}`);
     await page.goto(LOGIN_PAGE).then(() => {
-        console.log(' -> success!')
+        console.log(' -> success!');
     });
+
 
     console.log('Filling login form with credentials');
     await page.type('#email', conf.login);
     await page.type('#password', conf.password);
-    await Promise.all([
-        page.click('#submit-button'),
-        page.waitForNavigation({waitUntil: 'networkidle0'})
-    ]).then(() => {
-        console.log(' -> success!')
-    });
+    await page.click('.grid--cell.s-btn.s-btn__primary.js-accept-cookies.js-consent-banner-hide')
+        .catch(e => console.log("Could not find 'Accept Cookies' button."));
+
+    try {
+        await Promise.all([
+            page.click('#submit-button'),
+            page.waitForNavigation({waitUntil: 'networkidle0'})
+        ]).then(async () => {
+            console.log(' -> success!');
+        });
+    } catch (error) {
+        console.error(error);
+        notify(`Login error. Could not login to StackOverflow.`);
+        await browser.close();
+        return;
+    }
+
 
     console.log('Loading profile page');
     await page.goto(PROFILE_PAGE).then(() => {
-        console.log(' -> success!')
+        console.log(' -> success!');
     });
 
     try {
         console.log('Grabbing stats');
-        await page.waitForSelector(statsSelector = '#top-cards span.fs-caption', { timeout: 5000 })
-              .then(() => {
-                  console.log(' -> success!')
-              });
+        await page.waitForSelector(statsSelector = '#top-cards span.fs-caption', {timeout: 5000})
+            .then(() => {
+                console.log(' -> success!');
+            });
 
         const stats = await page.evaluate(
-        statsSelector => document.querySelector(statsSelector).textContent,
-        statsSelector
-    );
+            statsSelector => document.querySelector(statsSelector).textContent,
+            statsSelector
+        );
 
-    console.log(`Sending stats: ${stats}`);
-    notify(`Logged in for ${stats} consecutive days`)
+        console.log(`Sending stats: ${stats}`);
+        notify(`Logged in for ${stats} consecutive days`)
 
-    } catch(e) {
-        console.log(' -> failed. Could not fetch the stats. Please check days left manually.')
+    } catch (e) {
+        console.log(' -> failed. Could not fetch the stats. Please check days left manually.');
         notify(`Login succeeded. Could not fetch stats for consecutive days.`)
     }
-    
+
 
     console.log('Closing browser');
     await browser.close();
@@ -84,10 +97,9 @@ async function crawl() {
 function job() {
     try {
         crawl();
-    }
-    catch (error) {
+    } catch (error) {
         console.error(error);
-        notify(`Something went wrong: ${error.toString()}`)
+        notify(`Something went wrong: ${error.toString()}`);
     }
 }
 
